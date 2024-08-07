e3ae56e9e33cf2fc6a7129bb702c4f4e107a7b67
Esdras <esdras.castillo@unah.hn>
2024-08-06 22:28:44 -0600

    Actualizacion

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:alcaldias/views/login.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'alcaldias',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: child,
+        );
+      },
+      child: const Scaffold(body: Login()),
+    );
+  }
+}
---
+++ lib/models/register_response.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+RegisterResponseModel registerResponseJson(String str) =>
+    RegisterResponseModel.fromJson(json.decode(str));
+
+class RegisterResponseModel {
+  RegisterResponseModel({
+    required this.message,
+    required this.data,
+  });
+  late final String message;
+  late final Data? data;
+
+  RegisterResponseModel.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    data = Data.fromJson(json['data']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['message'] = message;
+    _data['data'] = data!.toJson();
+    return _data;
+  }
+}
+
+class Data {
+  Data({
+    required this.username,
+    required this.password,
+  });
+  late final String username;
+  late final String password;
+
+  Data.fromJson(Map<String, dynamic> json) {
+    username = json['username'];
+    password = json['password'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['username'] = username;
+    _data['password'] = password;
+    return _data;
+  }
+}
